bd6f968a693feed0494dcf62ec2bcdd76b602478
JJ Gray <jjgray@JJs-MacBook-Pro.local>
2020-03-18 22:13:41 +0000

    for loop for button printing & pseudocode for next step

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const capitalCities = {
   Afghanistan: "Kabul",
   Albania: "Tirana",
   Algeria: "Alger",
-  "American Samoa": "Fagatogo",
+  "American-Samoa": "Fagatogo",
   Andorra: "Andorra la Vella",
   Angola: "Luanda",
   Anguilla: "The Valley",
-  "Antigua and Barbuda": "Saint John's",
+  "Antigua-and-Barbuda": "Saint John's",
   Argentina: "Buenos Aires",
   Armenia: "Yerevan",
   Aruba: "Oranjestad",
@@ -24,54 +24,54 @@ const capitalCities = {
   Bermuda: "Hamilton",
   Bhutan: "Thimphu",
   Bolivia: "La Paz",
-  "Bosnia and Herzegovina": "Sarajevo",
+  "Bosnia-and-Herzegovina": "Sarajevo",
   Botswana: "Gaborone",
   Brazil: "Brasília",
   Brunei: "Bandar Seri Begawan",
   Bulgaria: "Sofia",
-  "Burkina Faso": "Ouagadougou",
+  "Burkina-Faso": "Ouagadougou",
   Burundi: "Bujumbura",
   Cambodia: "Phnom Penh",
   Cameroon: "Yaound",
   Canada: "Ottawa",
-  "Cape Verde": "Praia",
-  "Cayman Islands": "George Town",
-  "Central African Republic": "Bangui",
+  "Cape-Verde": "Praia",
+  "Cayman-Islands": "George Town",
+  "Central-African-Republic": "Bangui",
   Chad: "N'Djam",
   Chile: "Santiago de Chile",
   China: "Peking",
-  "Christmas Island": "Flying Fish Cove",
-  "Cocos (Keeling) Islands": "West Island",
+  "Christmas-Island": "Flying Fish Cove",
+  "Cocos-(Keeling)-Islands": "West Island",
   Colombia: "Santaf",
   Comoros: "Moroni",
   Congo: "Brazzaville",
-  "Cook Islands": "Avarua",
-  "Costa Rica": "San Jose",
+  "Cook-Islands": "Avarua",
+  "Costa-Rica": "San Jose",
   Croatia: "Zagreb",
   Cuba: "La Habana",
   Cyprus: "Nicosia",
-  "Czech Republic": "Praha",
+  "Czech-Republic": "Praha",
   Denmark: "Copenhagen",
   Djibouti: "Djibouti",
   Dominica: "Roseau",
-  "Dominican Republic": "Santo Domingo de Guzm",
-  "East Timor": "Dili",
+  "Dominican-Republic": "Santo Domingo de Guzm",
+  "East-Timor": "Dili",
   Ecuador: "Quito",
   Egypt: "Cairo",
-  "El Salvador": "San Salvador",
+  "El-Salvador": "San Salvador",
   England: "London",
-  "Equatorial Guinea": "Malabo",
+  "Equatorial-Guinea": "Malabo",
   Eritrea: "Asmara",
   Estonia: "Tallinn",
   Ethiopia: "Addis Abeba",
-  "Falkland Islands": "Stanley",
-  "Faroe Islands": "Tórshavn",
-  "Fiji Islands": "Suva",
+  "Falkland-Islands": "Stanley",
+  "Faroe-Islands": "Tórshavn",
+  "Fiji-Islands": "Suva",
   Finland: "Helsinki",
   France: "Paris",
-  "French Guiana": "Cayenne",
-  "French Polynesia": "Papeete",
-  "French Southern and Antarctic Lands": "Saint Pierre",
+  "French-Guiana": "Cayenne",
+  "French-Polynesia": "Papeete",
+  "French-Southern and Antarctic Lands": "Saint Pierre",
   Gabon: "Libreville",
   Gambia: "Banjul",
   Georgia: "Tbilisi",
@@ -88,9 +88,9 @@ const capitalCities = {
   "Guinea-Bissau": "Bissau",
   Guyana: "Georgetown",
   Haiti: "Port-au-Prince",
-  "Holy See (Vatican City State)": "Citt",
+  "Holy-See-(Vatican-City-State)": "Citt",
   Honduras: "Tegucigalpa",
-  "Hong Kong": "Victoria",
+  "Hong-Kong": "Victoria",
   Hungary: "Budapest",
   Iceland: "Reykjav",
   India: "New Delhi",
@@ -100,7 +100,7 @@ const capitalCities = {
   Ireland: "Dublin",
   Israel: "Jerusalem",
   Italy: "Roma",
-  "Ivory Coast": "Yamoussoukro",
+  "Ivory-Coast": "Yamoussoukro",
   Jamaica: "Kingston",
   Japan: "Tokyo",
   Jordan: "Amman",
@@ -114,25 +114,25 @@ const capitalCities = {
   Lebanon: "Beirut",
   Lesotho: "Maseru",
   Liberia: "Monrovia",
-  "Libyan Arab Jamahiriya": "Tripoli",
+  "Libyan-Arab-Jamahiriya": "Tripoli",
   Liechtenstein: "Vaduz",
   Lithuania: "Vilnius",
   Luxembourg: "Luxembourg [Luxemburg/L",
   Macao: "Macao",
-  "North Macedonia": "Skopje",
+  "North-Macedonia": "Skopje",
   Madagascar: "Antananarivo",
   Malawi: "Lilongwe",
   Malaysia: "Kuala Lumpur",
   Maldives: "Male",
   Mali: "Bamako",
   Malta: "Valletta",
-  "Marshall Islands": "Dalap-Uliga-Darrit",
+  "Marshall-Islands": "Dalap-Uliga-Darrit",
   Martinique: "Fort-de-France",
   Mauritania: "Nouakchott",
   Mauritius: "Port-Louis",
   Mayotte: "Mamoutzou",
   Mexico: "Ciudad de M",
-  "Microne, Federated States of": "Palikir",
+  "Federated-States-of-Microne": "Palikir",
   Moldova: "Chisinau",
   Monaco: "Monaco-Ville",
   Mongolia: "Ulan Bator",
@@ -144,63 +144,63 @@ const capitalCities = {
   Nauru: "Yaren",
   Nepal: "Kathmandu",
   Netherlands: "Amsterdam",
-  "Netherlands Antilles": "Willemstad",
-  "New Caledonia": "Noum",
-  "New Zealand": "Wellington",
+  "Netherlands-Antilles": "Willemstad",
+  "New-Caledonia": "Noum",
+  "New-Zealand": "Wellington",
   Nicaragua: "Managua",
   Niger: "Niamey",
   Nigeria: "Abuja",
   Niue: "Alofi",
-  "Norfolk Island": "Kingston",
-  "North Korea": "Pyongyang",
-  "Northern Ireland": "Belfast",
-  "Northern Mariana Islands": "Garapan",
+  "Norfolk-Island": "Kingston",
+  "North-Korea": "Pyongyang",
+  "Northern-Ireland": "Belfast",
+  "Northern-Mariana-Islands": "Garapan",
   Norway: "Oslo",
   Oman: "Masqat",
   Pakistan: "Islamabad",
   Palau: "Koror",
   Palestine: "Gaza",
   Panama: "Ciudad de Panam",
-  "Papua New Guinea": "Port Moresby",
+  "Papua-New-Guinea": "Port Moresby",
   Paraguay: "Asunci",
   Peru: "Lima",
   Philippines: "Manila",
   Pitcairn: "Adamstown",
   Poland: "Warszawa",
   Portugal: "Lisboa",
-  "Puerto Rico": "San Juan",
+  "Puerto-Rico": "San Juan",
   Qatar: "Doha",
   Reunion: "Saint-Denis",
   Romania: "Bucuresti",
-  "Russian Federation": "Moscow",
+  "Russian-Federation": "Moscow",
   Rwanda: "Kigali",
-  "Saint Helena": "Jamestown",
-  "Saint Kitts and Nevis": "Basseterre",
-  "Saint Lucia": "Castries",
-  "Saint Pierre and Miquelon": "Saint-Pierre",
-  "Saint Vincent and the Grenadines": "Kingstown",
+  "Saint-Helena": "Jamestown",
+  "Saint-Kitts and Nevis": "Basseterre",
+  "Saint-Lucia": "Castries",
+  "Saint-Pierre and Miquelon": "Saint-Pierre",
+  "Saint-Vincent and the Grenadines": "Kingstown",
   Samoa: "Apia",
-  "San Marino": "San Marino",
-  "Sao Tome and Principe": "Sao Tome",
-  "Saudi Arabia": "Riyadh",
+  "San-Marino": "San Marino",
+  "Sao-Tome-an-Principe": "Sao Tome",
+  "Saudi-Arabia": "Riyadh",
   Scotland: "Edinburgh",
   Senegal: "Dakar",
   Seychelles: "Victoria",
-  "Sierra Leone": "Freetown",
+  "Sierra-Leone": "Freetown",
   Singapore: "Singapore",
   Slovakia: "Bratislava",
   Slovenia: "Ljubljana",
-  "Solomon Islands": "Honiara",
+  "Solomon-Islands": "Honiara",
   Somalia: "Mogadishu",
-  "South Africa": "Pretoria",
-  "South Georgia and the South Sandwich Islands": "King Edward Point",
-  "South Korea": "Seoul",
-  "South Sudan": "Juba",
+  "South-Africa": "Pretoria",
+  "South-Georgia and the South Sandwich Islands": "King Edward Point",
+  "South-Korea": "Seoul",
+  "South-Sudan": "Juba",
   Spain: "Madrid",
-  "Sri Lanka": "Colombo",
+  "Sri-Lanka": "Colombo",
   Sudan: "Khartum",
   Suriname: "Paramaribo",
-  "Svalbard and Jan Mayen": "Longyearbyen",
+  "Svalbard-and-Jan-Mayen": "Longyearbyen",
   Swaziland: "Mbabane",
   Sweden: "Stockholm",
   Switzerland: "Bern",
@@ -208,31 +208,31 @@ const capitalCities = {
   Tajikistan: "Dushanbe",
   Tanzania: "Dodoma",
   Thailand: "Bangkok",
-  "The Democratic Republic of Congo": "Kinshasa",
+  "The-Democratic-Republic-of-Congo": "Kinshasa",
   Togo: "Lom",
   Tokelau: "Fakaofo",
   Tonga: "Nuku'alofa",
-  "Trinidad and Tobago": "Port-of-Spain",
+  "Trinidad-and-Tobago": "Port-of-Spain",
   Tunisia: "Tunis",
   Turkey: "Ankara",
   Turkmenistan: "Ashgabat",
-  "Turks and Caicos Islands": "Cockburn Town",
+  "Turks-and-Caicos-Islands": "Cockburn Town",
   Tuvalu: "Funafuti",
   Uganda: "Kampala",
   Ukraine: "Kyiv",
-  "United Arab Emirates": "Abu Dhabi",
-  "United Kingdom": "London",
-  "United States": "Washington",
+  "United-Arab Emirates": "Abu Dhabi",
+  "United-Kingdom": "London",
+  "United-States": "Washington",
   Uruguay: "Montevideo",
   Uzbekistan: "Toskent",
   Vanuatu: "Port-Vila",
   Venezuela: "Caracas",
   Vietnam: "Hanoi",
-  "Virgin Island, British": "Road Town",
-  "Virgin Island, U.S.": "Charlotte Amalie",
+  "Virgin-Island-British": "Road Town",
+  "Virgin-Island-U.S.": "Charlotte Amalie",
   Wales: "Cardiff",
-  "Wallis and Futuna": "Mata-Utu",
-  "Western Sahara": "El-Aai",
+  "Wallis-and-Futuna": "Mata-Utu",
+  WesternSahara: "El-Aai",
   Yemen: "Sanaa",
   Yugoslavia: "Beograd",
   Zambia: "Lusaka",
@@ -244,7 +244,6 @@ const getRandomCities = obj => {
   const countries = Object.keys(obj);
   //  Random country from array
   const randomCountry = countries[Math.floor(countries.length * Math.random())];
-
   return obj[randomCountry];
 };
 
@@ -260,43 +259,55 @@ const getRandomCountry = obj => {
 let randomCountry = getRandomCountry(capitalCities);
 let randomCity = capitalCities[randomCountry];
 
-const refreshAnswer = () => {
+const correctAnswer = () => {
   randomCountry = getRandomCountry(capitalCities);
   randomCity = capitalCities[randomCountry];
+  `<div><button onclick=getScore("${cityToPrint}")>${cityToPrint}</button></div>`;
 };
 
-const getNewInputs = () => {
-  refreshAnswer();
+let mockAnswer = "London";
 
-  document.getElementById("country-card").innerHTML = randomCountry;
+// const getNewInputs = () => {
+//   refreshAnswer();
 
-  document.getElementById("box-one").innerHTML = getRandomCities(capitalCities);
+//   document.getElementById("box-four").innerHTML = randomCity;
+// };
 
-  document.getElementById("box-two").innerHTML = getRandomCities(capitalCities);
+// getNewInputs();
 
-  document.getElementById("box-three").innerHTML = getRandomCities(
+const createButtons = () => {
+  let output = "";
+  for (let index = 0; index < 3; index++) {
+    let cityToPrint = getRandomCities(capitalCities);
+    output += `<div><button onclick=getScore("${cityToPrint}")>${cityToPrint}</button></div>`;
+  }
+  return output;
+};
+
+const printCountry = () =>
+  (document.getElementById("country-card").innerHTML = getRandomCountry(
     capitalCities
-  );
-  document.getElementById("box-four").innerHTML = randomCity;
-};
+  ));
+
+const printButtons = () =>
+  (document.getElementById("answer-boxes").innerHTML = createButtons());
+
+printCountry();
+printButtons();
 
 let score = 0;
-let countries = Object.keys(capitalCities);
-let cities = Object.values(capitalCities);
 
-const getScore = () => {
-  if (
-    Object.keys(
-      capitalCities[document.getElementById("country-card").innerHTML]
-    ) ==
-    Object.values(capitalCities[document.getElementById("box-four").innerHTML])
-  ) {
+const getScore = city => {
+  if (city === mockAnswer) {
     score++;
-  } else {
-    score;
   }
+
+  console.log(city);
+
+  score;
   updateHtmlScore();
-  getNewInputs();
+  printCountry();
+  printButtons();
 };
 
 const updateHtmlScore = () => {
@@ -304,11 +315,15 @@ const updateHtmlScore = () => {
 };
 
 updateHtmlScore();
-getNewInputs();
 
-const createButtons = () => {};
-// let index = 0;
-for (let index = 0; index < 4; index++) {
-  // getRandomCities(capitalCities);
-  console.log(getRandomCities(capitalCities));
-}
+let arr = [1, 2, 3];
+
+console.log(shuffle(arr));
+
+
+// for loop to get 3 x random cities - push into an Array
+// spread operator to create new version of that array, and put the correct city into it
+// math.random function to shuffle the order of the array
+// map over array and apply html string
+// print buttons function
+
